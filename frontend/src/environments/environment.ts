7b5f03aa7169d3043057e164150bb3eb4a5646a5
Luis Salazar <bp.lusv@gmail.com>
2020-08-31 16:12:13 -0600

    Update API endpoints

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-runifpea.us', // the auth0 domain prefix
+    audience: 'api', // the audience set for the auth0 app
+    clientId: 'AhdYwaSKUJkg9MlOyqBOFM9x7XO08le1', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
